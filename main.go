1197820180c1167d484d2067ba15ab85addf6a2e
BarryYan <barry.yansh@gmail.com>
2019-08-10 00:11:49 +0800

    Parse sub thread

--- main.go
+++ main.go
@@ -11,21 +11,81 @@ import (
 func main() {
 	file, err := ioutil.ReadFile(`./222.log`)
 	if err == nil {
-		mainThreadKeys := matchMainThread(string(file))
-		fmt.Println(mainThreadKeys)
+		content := string(file)
+		mainThread := matchMainThread(content)
+		for key, dateTotal := range mainThread {
+			fmt.Println(key, dateTotal)
+		}
+
+		fmt.Println("-----")
+
+		subThread := matchSubThread(content)
+		for pid, keyDate := range subThread {
+			for key, date := range keyDate {
+				fmt.Println(pid, key, date)
+			}
+		}
 	}
 }
 
-func matchMainThread(file string) map[string]string {
-	reg := regexp.MustCompile(`MAIN_(\S+)\]\s+time:(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}.\d{3})`)
-	// mathed := reg.FindStringSubmatch(text)
-	mathes := reg.FindAllStringSubmatch(file, -1)
-	result := map[string]string{}
-	for _, line := range mathes {
-		result[line[1]] = line[2]
-		fmt.Println(line[1], line[2])
+func matchMainThread(file string) map[string][]string {
+	timeKeyReg := regexp.MustCompile(`MAIN_(\w+)\]\s+time:(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}.\d{3})`)
+	timeKeyMatched := timeKeyReg.FindAllStringSubmatch(file, -1)
+
+	timeKeyTotalLineReg := regexp.MustCompile(`.*MAIN_TIME.*\s(\S+)`)
+	timeKeyTotalLineMatched := timeKeyTotalLineReg.FindStringSubmatch(file)
+
+	timeKeyTotalReg := regexp.MustCompile(`(\w+):(\d+)`)
+	timeKeyTotalMatched := timeKeyTotalReg.FindAllStringSubmatch(timeKeyTotalLineMatched[1], -1)
+
+	mainTread := map[string][]string{}
+	for _, keyDate := range timeKeyMatched {
+		mainTread[keyDate[1]] = []string{keyDate[2]}
+		fmt.Println(keyDate[1], mainTread[keyDate[1]])
 	}
-	return result
+	fmt.Println("-----")
+
+	for _, keyTotal := range timeKeyTotalMatched {
+		key, total := keyTotal[1], keyTotal[2]
+		mainTread[key] = append(mainTread[key], total)
+	}
+
+	return mainTread
+}
+
+func matchSubThread(file string) map[string]map[string][]string {
+	timeKeyReg := regexp.MustCompile(`SUB_(\w+)_(\w+)\]\s+time:(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}.\d{3})`)
+	timeKeyMatched := timeKeyReg.FindAllStringSubmatch(file, -1)
+
+	subThread := map[string]map[string][]string{}
+	for _, pidKeyDate := range timeKeyMatched {
+		pid, key, date := pidKeyDate[1], pidKeyDate[2], pidKeyDate[3]
+		// fmt.Println(pid, key, date)
+		if _, pidOk := subThread[pid]; pidOk {
+			if _, keyOk := subThread[pid][key]; !keyOk {
+				subThread[pid][key] = []string{date}
+			}
+		} else {
+			subThread[pid] = map[string][]string{key: []string{date}}
+		}
+	}
+
+	timeTotalKeyLineReg := regexp.MustCompile(`.*SUB_TIME_(\d+)\S+\s(.*)`)
+	timeTotalKeyLineMatched := timeTotalKeyLineReg.FindAllStringSubmatch(file, -1)
+
+	timeKeyTotalReg := regexp.MustCompile(`(\w+):(\d+)`)
+	for _, keyTotalLineMatched := range timeTotalKeyLineMatched {
+		pid, keyTotalLine := keyTotalLineMatched[1], keyTotalLineMatched[2]
+		timeKeyTotalMatched := timeKeyTotalReg.FindAllStringSubmatch(keyTotalLine, -1)
+		for _, keyTotal := range timeKeyTotalMatched {
+			key, total := keyTotal[1], keyTotal[2]
+			if _, ok := subThread[pid][key]; ok {
+				subThread[pid][key] = append(subThread[pid][key], total)
+			}
+		}
+	}
+
+	return subThread
 }
 
 func createExcel() {
